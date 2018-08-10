8f21521b2e77d02313d9364fba0d78f64d7b9d2f
tanguyen <tam.nguyen@datalogic.com>
2018-08-10 15:26:51 +0700

    + Create concrete class + Add class diagram

--- FactoryPattern/FactoryPattern.cpp
+++ FactoryPattern/FactoryPattern.cpp
@@ -3,93 +3,120 @@
 
 #include "stdafx.h"
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <list>
 
-class Pizza
+using namespace std;
+
+class APizza
 {
 public:
-    Pizza()
+    APizza()
     {
+        name = "";
+        dough = "";
+        sauce = "";
         printf("Pizza class\n");
     }
 
     bool prepare() 
     { 
-        printf("=>do prepare\n");
+        printf("Preparing %s\n", name);
+        printf("Tossing dough...\n");
+        printf("Adding sauce...\n");
+        printf("Adding toppings: \n");
+
+        for (int i = 0; i < 3; i++)
+        {
+            //
+        }
+
         return true;
     }
 
     bool bake() 
     {
-        printf("=>do bake\n");
+        printf("Bake for 25 mins at 350\n");
         return true; 
     }
 
     bool cut()
     {
-        printf("=>do cut\n");
+        printf("Cutting the pizza into diagonal slices\n");
         return true;
     }
 
     bool box()
     {
-        printf("=>do box\n");
+        printf("Place pizza in official PizzaStote Box\n");
         return true;
     }
+
+    char * GetName()
+    {
+        return name;
+    }
+    
+protected:
+    char * name;
+    char * dough;
+    char * sauce;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class CheesePizza : public Pizza
-{
-public:
-    CheesePizza()
-    {
-        printf("CheesePizza class\n");
-    }
-};
+//class CheesePizza : public APizza
+//{
+//public:
+//    CheesePizza()
+//    {
+//        printf("CheesePizza class\n");
+//    }
+//};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class PepperoniPizza : public Pizza
-{
-public:
-    PepperoniPizza()
-    {
-        printf("PepperoniPizza class\n");
-    }
-};
+//class PepperoniPizza : public APizza
+//{
+//public:
+//    PepperoniPizza()
+//    {
+//        printf("PepperoniPizza class\n");
+//    }
+//};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class VeggiePizza : public Pizza
-{
-public:
-    VeggiePizza()
-    {
-        printf("VeggiePizza class\n");
-    }
-};
+//class VeggiePizza : public APizza
+//{
+//public:
+//    VeggiePizza()
+//    {
+//        printf("VeggiePizza class\n");
+//    }
+//};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class SimplePizzaFactory
 {
 public:
-    Pizza * CreatePizza(char * type)
+    APizza * CreatePizza(char * type)
     {
-        Pizza * pizza = NULL;
+        APizza * pizza = NULL;
 
         if (type == "cheese")
         {
-            pizza = new CheesePizza();
+            //pizza = new CheesePizza();
         }
         else if (type == "pepperoni")
         {
-            pizza = new PepperoniPizza();
+            //pizza = new PepperoniPizza();
         }
         else if (type == "veggie")
         {
-            pizza = new VeggiePizza();
+            //pizza = new VeggiePizza();
         }
 
         return pizza;
@@ -103,9 +130,9 @@ class APizzaStore
 private:
     //SimplePizzaFactory factory;
 public:
-    Pizza * OrderPizza(char * type)
-    {
-        Pizza *pizza;
+    APizza * OrderPizza(char * type)
+     {
+        APizza *pizza;
 
         pizza = CreatePizza(type);
 
@@ -118,7 +145,62 @@ public:
     }
 
 protected:
-    virtual Pizza * CreatePizza(char * type) = 0; // abstract method (pure virtual function), the sub classes must implement this one.
+    virtual APizza * CreatePizza(char * type) = 0; // abstract method (pure virtual function), the sub classes must implement this one.
+    bool isType(char * inputType, char * myType)
+    {
+        bool bRet = false;
+
+        if (strlen(inputType) == strlen(myType))
+        {
+            if (strcmp(inputType, myType) == 0)
+                bRet = true;
+        }
+
+        return bRet;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class NYStyleCheesePizza : public APizza
+{
+public:
+    NYStyleCheesePizza()
+    {
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class NYStylePepperoniPizza : public APizza
+{
+public:
+    NYStylePepperoniPizza()
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class NYStyleStyleClamPizza : public APizza
+{
+public:
+    NYStyleStyleClamPizza()
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class NYStyleVeggiePizza : public APizza
+{
+public:
+    NYStyleVeggiePizza()
+    {
+    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,28 +213,68 @@ public:
         printf("This is NYStylePizzaStore\n");
     }
 
-    Pizza * CreatePizza(char * type)
+    APizza * CreatePizza(char * type)
     {
-        Pizza * pizza = NULL;
+        APizza * pizza = NULL;
         printf("NYStylePizza is created\n");
-        //if (type == "cheese")
-        //{
-        //    pizza = new NYStyleCheesePizza();
-        //}
-        //else if (type == "pepperoni")
-        //{
-        //    pizza = new NYStylePepperoniPizza();
-        //}
-        //else if (type == "clam")
-        //{
-        //    pizza = new NYStyleStyleClamPizza();
-        //}
-        //else if (type == "veggie")
-        //{
-        //    pizza = new NYStyleVeggiePizza();
-        //}
+        if (isType(type, "cheese") == true)
+        {
+            pizza = new NYStyleCheesePizza();
+        }
+        else if (isType(type, "pepperoni") == true)
+        {
+            pizza = new NYStylePepperoniPizza();
+        }
+        else if (isType(type, "clam") == true)
+        {
+            pizza = new NYStyleStyleClamPizza();
+        }
+        else if (isType(type, "veggie") == true)
+        {
+            pizza = new NYStyleVeggiePizza();
+        }
 
         return pizza;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ChicagoStyleCheesePizza : public APizza
+{
+public:
+    ChicagoStyleCheesePizza()
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ChicagoStylePepperoniPizza : public APizza
+{
+public:
+    ChicagoStylePepperoniPizza()
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ChicagoStyleClamPizza : public APizza
+{
+public:
+    ChicagoStyleClamPizza()
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ChicagoStyleVeggiePizza : public APizza
+{
+public:
+    ChicagoStyleVeggiePizza()
+    {
     }
 };
 
@@ -166,42 +288,50 @@ public:
         printf("This is ChicagoStylePizzaStore\n");
     }
 
-    Pizza * CreatePizza(char * type)
+    APizza * CreatePizza(char * type)
     {
-        Pizza * pizza = NULL;
+        APizza * pizza = NULL;
+
         printf("ChicagoStylePizza is created\n");
 
-        //if (type == "cheese")
-        //{
-        //    pizza = new ChicagoStyleCheesePizza();
-        //}
-        //else if (type == "pepperoni")
-        //{
-        //    pizza = new ChicagoStylePepperoniPizza();
-        //}
-        //else if (type == "clam")
-        //{
-        //    pizza = new ChicagoStyleClamPizza();
-        //}
-        //else if (type == "veggie")
-        //{
-        //    pizza = new ChicagoStyleVeggiePizza();
-        //}
+        if (isType(type, "cheese") == true)
+        {
+            pizza = new ChicagoStyleCheesePizza();
+        }
+        else if (isType(type, "pepperoni") == true)
+        {
+            pizza = new ChicagoStylePepperoniPizza();
+        }
+        else if (isType(type, "clam") == true)
+        {
+            pizza = new ChicagoStyleClamPizza();
+        }
+        else if (isType(type, "veggie") == true)
+        {
+            pizza = new ChicagoStyleVeggiePizza();
+        }
 
         return pizza;
     }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool PizzaTestDrive()
+{
+    APizzaStore * pNYStylePizzaStore = new NYStylePizzaStore();
+    APizza * pPizza = pNYStylePizzaStore->OrderPizza("cheese");
+
+    //APizzaStore * pChicagoStypePizza = new ChicagoStylePizzaStore();
+    //pChicagoStypePizza->OrderPizza("cheese");
+
+    return false;
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    ChicagoStylePizzaStore * pChicagoStypePizza = new ChicagoStylePizzaStore();
-    NYStylePizzaStore * pNYStylePizza = new NYStylePizzaStore();
-
-    pChicagoStypePizza->OrderPizza("cheese");
-    pNYStylePizza->OrderPizza("cheese");
+    PizzaTestDrive();
 
     return 0;
 }
